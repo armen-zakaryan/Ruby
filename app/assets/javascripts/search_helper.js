89c92a0d5604f36fa3f155a7e99bcb5df9ad0fc0
Armen Zakaryan <azakaryan@macadamian.com>
2015-08-20 20:33:04 +0400

    added search in angular

--- app/assets/javascripts/search_helper.js
+++ app/assets/javascripts/search_helper.js
@@ -2,23 +2,63 @@
  * Created by azakaryan on 13.08.2015.
  */
 
-var self = this;
-
-$(document).ready(
-    function () {
-        $('#h-search-btn').on('click', function(event){
-        $('.h-search').toggleClass('content-visible');
-    });
-});
 
 
 
-var app = angular.module('myApp', []);
 
-app.controller('SearchCtrl', ['$scope', function($scope) {
+var app = angular.module('myApp', ['ngResource']);
 
-    $scope.submit = function(){
-        debugger;
-        $scope.key
+
+//*****************
+//**** Models *****
+//*****************
+app.service('HotelsSvc', ['$resource', function($resource){
+    var actions = {
+        getAll: {
+            method: 'GET',
+            params: {},
+            cache: true,
+            isArray: true,
+            transformResponse: function(data) {
+                if (typeof data === 'string'){
+                    return JSON.parse(data)
+                } else {
+                    return data
+                };
+            }
+        }
+    };
+
+    return $resource('/api/v1/auth', {}, actions);
+}]);
+//*****************
+//** Controllers **
+//*****************
+app.controller('SearchCtrl', ['$scope', 'HotelsSvc', function($scope, HotelsSvc) {
+    $scope.title = "Advanced Search";
+    $scope.isSearchVisible = false;
+    $scope.hotels = [];
+
+    $scope.toggleSearch = function(){
+        $scope.isSearchVisible = !$scope.isSearchVisible;
+    }
+
+    $scope.search = function(){
+        HotelsSvc.getAll().$promise.then(function(data){
+            $scope.hotels = data;
+        });
     }
 }]);
+
+
+
+
+
+/*
+ $(document).ready(
+ function () {
+ $('#h-search-btn').on('click', function(event){
+ $('.h-search').toggleClass('content-visible');
+ });
+ });
+ */
